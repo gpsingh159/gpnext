5dd2a58af7ecc5aefed2747a471cea6b7dc71f46
gurpreet singh <gurpreet.singh@ecosmob.com>
2023-04-24 15:52:38 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
-import Helmet from "react-helmet";
+// import Helmet from "react-helmet";
 
 const inter = Inter({ subsets: ["latin"] });
 
